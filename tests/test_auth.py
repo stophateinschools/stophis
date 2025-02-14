1792a6c753303c16ca6fbad4bc5fe0a95cdb5a8c
Anna Rusk <anna@joon.com>
2025-02-14 13:27:02 -0800

    Try to setup pytest with postgresql test db

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,39 @@
-def func(x):
-    return x + 1
+import pytest
 
+from server.user import UserRole, User, Role
+from server import db
 
-def test_answer():
-    assert func(3) == 4
+@pytest.fixture
+def admin_role(db_session):
+    admin_role = Role(name=UserRole.ADMIN)
+    db_session.add(admin_role)
+    db_session.commit()
+    return admin_role
+
+@pytest.fixture
+def editor_role(db_session):
+    editor_role = Role(name="Aditor")
+    db_session.add(editor_role)
+    db_session.commit()
+    return editor_role
+
+@pytest.fixture
+def user_admin(db_session, admin_role):
+    user = User(
+        first_name="Test",
+        last_name="User",
+        email="user@test.com",
+    )
+
+    user.roles.append(admin_role)
+
+    db_session.add(user)
+    db_session.commit()
+    return user
+
+def test_get_users(db_session, admin_role, user_admin):
+    user = db_session.query(User).filter_by(email='user@test.com').first()
+    user2 = db_session.query(User).filter_by(email='user@.com').first()
+
+    assert user.roles[0] == admin_role
+    assert user2 is None
