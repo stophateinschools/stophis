ac4c83c359a8abc19ec582028ea12725243a0700
Anna Rusk <anna@joon.com>
2025-05-20 15:53:40 -0700

    Sync up our tox setup with Docker (no more pipenv)

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
 import pytest
 
+from server.models.models import AttributionType
 from server.models.user import UserRole, User, Role
 from server import db
 
@@ -13,6 +14,14 @@ def admin_role(db_session):
 
 
 @pytest.fixture
+def stop_hate_attribution(db_session):
+    stop_hate_attribution = AttributionType(name="Stop Hate in Schools")
+    db_session.add(stop_hate_attribution)
+    db_session.commit()
+    return stop_hate_attribution
+
+
+@pytest.fixture
 def editor_role(db_session):
     editor_role = Role(name="Aditor")
     db_session.add(editor_role)
@@ -21,11 +30,12 @@ def editor_role(db_session):
 
 
 @pytest.fixture
-def user_admin(db_session, admin_role):
+def user_admin(db_session, admin_role, stop_hate_attribution):
     user = User(
         first_name="Test",
         last_name="User",
         email="user@test.com",
+        attribution_type=stop_hate_attribution,
     )
 
     user.roles.append(admin_role)
