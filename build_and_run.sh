769f70bc8b5a200c952639632e3a77188485c4bc
Anna Rusk <anna@joon.com>
2025-05-21 15:24:57 -0700

    Test removing the root package-lock.json from commit

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,10 +2,8 @@
 
 # Build React frontend
 echo "Building client app..."
-cd client
 npm install
-npm run build
-cd ..
+npm run --workspace client build
 
 # Now run Flask app
 echo "Starting gunicorn..."
